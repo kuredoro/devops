fa121723fe8a05e24a3166bfb92292511eae5821
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-09-13 20:31:02 +0300

    Flask app logs to the gunicorn logger.

--- app_python/app.py
+++ app_python/app.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, timezone, timedelta
 from flask import Flask
 app = Flask(__name__)
@@ -5,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def display():
+    app.logger.info("Time requested")
     now = datetime.now(timezone(timedelta(hours=3)))
     return "{}-{}-{} {}:{}:{}.{}".format(
             now.year,
@@ -15,6 +17,13 @@ def display():
             now.second,
             now.microsecond)
 
+
+if __name__ != '__main__':
+    # if we are not running directly, we set the loggers
+    # Run as `gunicorn --log-level={debug,info,warning,error} app:app``
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
